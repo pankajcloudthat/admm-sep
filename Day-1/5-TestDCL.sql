71163d0fd401c9d69f167598c75f5e69f4a339af
Pankaj Choudhary <93825810+pankajcloudthat@users.noreply.github.com>
2026-08-31 15:33:45 +0530

    Update 5-TestDCL.sql

--- Day-1/5-TestDCL.sql
+++ Day-1/5-TestDCL.sql
@@ -1,14 +1,13 @@
 -- Active: 1770636209703@@127.0.0.1@3306@demo
-SHOW DATABASES;
-
 use ecommerce;
-
-SHOW TABLES;
-
-SELECT * FROM users;
-
-SELECT * FROM posts;
-
-
+select * from users;
 
 delete from users where id = 2;
+
+show tables;
+
+
+set role TestRole_ReadOnly;
+
+use ecommerce;
+select * from users;
